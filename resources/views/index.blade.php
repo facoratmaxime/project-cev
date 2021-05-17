e8728106e2688201bbf6f3da0490ba0393930236
Maxime FACORAT <facorat.maxime@gmail.com>
2021-05-18 00:07:18 +0200

    Fix CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,12 +10,12 @@
     <div class=" flex items-top justify-center bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         <div class="row">
             <div class="col-sm-12">
-                <form action="{{ route('process.upload') }}" method="POST"
-                    enctype="multipart/form-data">
+                <form action="{{ route('process.upload') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="form-group">
                         <div class="custom-file">
-                            <input accept=".csv" required type="file" class="custom-file-input" id="customFile" name="customFile">
+                            <input accept=".csv" required type="file" class="custom-file-input" id="customFile"
+                                name="customFile">
                             <label class="custom-file-label" for="customFile">Uploader votre fichier ici</label>
                         </div>
                     </div>
@@ -28,10 +28,10 @@
     </div>
 
     <div class="container">
-    <div class="row">
-        <a class="btn btn-primary" href="{{ route('process.upload_without_upload') }}">Utiliser le CSV par défaut</a>
 
-    <button class="btn btn-warning">Lien Github <a href="https://github.com/facoratmaxime/project-cev">ici</a></button>
-</div>
-</div>
+            <a class="btn btn-primary" href="{{ route('process.upload_without_upload') }}">Utiliser le CSV par défaut</a>
+
+            <a href="https://github.com/facoratmaxime/project-cev" target="_blank" class="btn btn-warning">Lien Github</a>
+    
+    </div>
 @endsection
